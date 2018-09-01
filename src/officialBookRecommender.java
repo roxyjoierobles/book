be34f43523879c4fcf2414a569aa8a445ba293f1
rroxanne <roxannejoier@gmail.com>
2018-08-31 17:01:36 -0700

    created option where you can input author name to ensure the correct book is inputted

--- src/officialBookRecommender.java
+++ src/officialBookRecommender.java
@@ -25,17 +25,24 @@ public class officialBookRecommender {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String book_title;
         System.out.print("Enter book title: ");
-        book_title = input.next();
+        String book_title = input.next();
         book_title += input.nextLine();
+        System.out.println("Enter author name. If you do not know, please input 'idk':" );
+        String book_author = input.next();
+        book_author += input.nextLine();
         //System.out.println("book entered: " + book_title);
         try {
-            URL_SOURCE = URL + URLEncoder.encode(book_title, "UTF-8");
+            if (book_author.equals("idk")) {
+                URL_SOURCE = URL + URLEncoder.encode(book_title, "UTF-8");
+            } else {
+                URL_SOURCE = URL_BASE + "author=" + URLEncoder.encode(book_author, "UTF-8") + "&key=" + KEY + "&title=" + URLEncoder.encode(book_title, "UTF-8");
+            }
             //System.out.println("url: " + URL_SOURCE);
             IBookParser parser = new XMLBookParser(URL_SOURCE);
             book = parser.parse();
-            System.out.println("You have read: " + book.getTitle());
+            System.out.println("You have read: " + book.getTitle() + " by " + book.getAuthor().getName());
+            System.out.println("please wait...");
             for (Book b : book.getSimilarBooks()) {
                 // System.out.println(b.getTitle());
                 // takes book titles and recursively calls bookhandler for each book in similar
@@ -51,7 +58,7 @@ public class officialBookRecommender {
             for (int i = 0; i < book.getSimilarBooks().size(); i++) {
                 bookRatings[i] = book.getSimilarBooks().get(i);
             }
-            System.out.println("\n books arranged from greatests to least rating");
+            System.out.println("Here are some books you can read, arranged from greatest to least rating");
             // following sorts list by insertion sort
             for (int j = 1; j < bookRatings.length - 1; j++) {
                 Book temp = bookRatings[j];
